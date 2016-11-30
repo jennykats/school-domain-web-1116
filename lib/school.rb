857a5030a4d8a39a4e5fb2479dc926b7a8334b70
Jenny <jenny.kats@aol.com>
2016-11-30 17:25:52 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,51 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :name, :roster
+  attr_reader :add_student
+
+  def initialize (name, roster ={})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student (student_name, grade)
+    if !roster.key?(grade)
+      roster[grade] = [student_name] # creating array & pushing
+    else
+      roster[grade] << student_name
+    end
+  end
+
+  def grade (grade)
+    if roster.key?(grade)
+      return roster[grade]
+    end
+  end
+
+  def sort
+    sorted_hash = roster.sort.to_h
+    sorted_hash.each do |k, v|
+      v.sort!
+    end
+
+  end
+
+
+
+
+
+end
+
+
+
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.roster
+
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
